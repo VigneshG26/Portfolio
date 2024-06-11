75f557029035a4fe4b9a03fbd9a91caffe0b2c5e
VigneshG26 <vigneshganesan2k1@gmail.com>
2024-06-11 15:50:07 +0530

    folder structure organized

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var links = document.querySelector('.links');
-    var hamburger = document.querySelector('.hamburger-box');
-    var sec3 = document.querySelector('.sec-3');
-    links.classList.toggle('active');
-    hamburger.classList.toggle('active');
-    sec3.classList.toggle('active');
-}
